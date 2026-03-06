d4e64e800dfcc3fb82386fd71a0bb1748d2d8ddf
revista1919 <revistaestudiantespentauc@gmail.com>
2026-03-06 14:21:32 -0300

    redimensionae

--- generate.js
+++ generate.js
@@ -192,7 +192,7 @@ function generateBibTeX(article) {
 }
 
 // ========== ICONOS SVG ==========
-const oaSvg = `<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24" height="36" viewBox="0 0 900 360">
+const oaSvg = `<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="20" height="20" viewBox="0 0 400 400">
   <g transform="matrix(1.25 0 0 -1.25 0 360)">
     <defs>
       <path id="a" d="M-90-36h900v360H-90z"/>
